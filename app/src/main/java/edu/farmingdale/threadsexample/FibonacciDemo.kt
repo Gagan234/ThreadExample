18ed2bcfcc3813565bcb3b4ce29cd1dd9ab6bbe8
Gagan <gagansapkota668@gmail.com>
2024-11-07 14:54:45 -0500

    TOdo 2 Done

--- app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
@@ -3,6 +3,7 @@ package edu.farmingdale.threadsexample
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -16,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.input.KeyboardType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.lang.reflect.Modifier
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -51,4 +53,37 @@ fun FibonacciDemoNoBgThrd() {
 
 fun fibonacci(n: Long): Long {
     return if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+@Composable
+fun FibonacciDemoBgThrd() {
+    var answer by remember { mutableStateOf("") }
+    var textInput by remember { mutableStateOf("40") }
+    var coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Spacer(modifier = Modifier.padding(60.dp))
+        Row {
+            TextField(
+                value = textInput,
+                onValueChange = { textInput = it },
+                label = { Text("Number?") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+            Button(onClick = {
+                coroutineScope.launch(Dispatchers.Default) {
+                val num = textInput.toLongOrNull() ?: 0
+                val fibNumber = fibonacci(num)
+                answer = NumberFormat.getNumberInstance(Locale.US).format(fibNumber)
+            }
+            }) {
+                Text("Fibonacci")
+            }
+        }
+
+        Text("Result: $answer")
+    }
 }
