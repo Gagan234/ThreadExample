d783eedea368dc5cf149e0d92037fa1e5e0d5ed4
Gagan <gagansapkota668@gmail.com>
2024-11-07 14:40:59 -0500

    TOdo 1 Done

--- app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
@@ -2,6 +2,7 @@ package edu.farmingdale.threadsexample
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -24,6 +25,7 @@ fun FibonacciDemoNoBgThrd() {
     var textInput by remember { mutableStateOf("40") }
 
     Column {
+        Spacer(modifier = Modifier.padding(60.dp))
         Row {
             TextField(
                 value = textInput,
